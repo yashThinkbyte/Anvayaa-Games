3a63378ac4e468ad4aac3e5ce67e7a43747a7b6d
yashThinkbyte <161916638+yashThinkbyte@users.noreply.github.com>
2024-10-25 01:58:21 +0530

    Update page.tsx

--- app/games/puzzleGrid/page.tsx
+++ app/games/puzzleGrid/page.tsx
@@ -1,17 +1,16 @@
 "use client";
-import React, { useState} from "react";
+import React from "react";
 import PuzzleGameBoard from "@/components/PuzzleGameBoard";
 
 const PuzzleGrid: React.FC = () => {
-
   return (
     <div className="app-container">
-          <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
-            <h1 className="text-center mb-4">Puzzle Grid Game</h1>
-            <div className="flex-1 flex items-center justify-center w-full max-h-full overflow-hidden">
-              <PuzzleGameBoard />
-            </div>
-          </div>
+      <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
+        <h1 className="text-center mb-4">Puzzle Grid Game</h1>
+        <div className="flex-1 flex items-center justify-center w-full max-h-full overflow-hidden">
+          <PuzzleGameBoard />
+        </div>
+      </div>
     </div>
   );
 };
